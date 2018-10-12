6eb556c09cc152785f18eaec85c3c7f0dffb9821
Tyler Clifford Sorenson <tylercs@maine.cs.byu.edu>
2018-10-12 13:01:28 -0600

    finish create table statements

--- server/server.sql
+++ server/server.sql
@@ -1,14 +1,37 @@
-create table user
-(
+create table users (
 	username text not null primary key,
 	password text not null,
 	name text not null,
-   email text not null,
+	email text not null,
 	firstName text not null,
 	lastName text not null,
-	
-	
-Gender: User’s gender (string: “f” or “m”)
-Person ID: Unique Person ID assigned to this user’s generated Person object - see Family
-History Information section for details (non-empty string)
+	gender text check (gender="f" or gender="m"),
+	personID text not null
+);
+
+create table tokens (
+	username text not null primary key,
+	authorizationToken text not null
+);
+
+create table persons (
+	personID text not null primary key,
+	descendantUsername text not null,
+	firstName text not null,
+	lastName text not null,
+	gender text check (gender="f" or gender="m"),
+	fatherID text,
+	motherID text,
+	spouseID text
+);
+
+create table events (
+	eventID text not null primary key,
+	descendantUsername text not null,
+	personID text not null,s
+	latitude real not null,
+	longitude real not null,
+	country text not null,
+	city text not null,
+	eventType text check (eventType="birth" or eventType="baptism" or eventType="christening" or eventType="marriage" or eventType="death")
 );
