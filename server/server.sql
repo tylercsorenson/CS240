ff0e115972f7557549eed45bb452ecc62366bfef
Tyler Clifford Sorenson <tylercs@maine.cs.byu.edu>
2018-10-12 13:20:23 -0600

    changes and updates to tables

--- server/server.sql
+++ server/server.sql
@@ -1,7 +1,6 @@
 create table users (
-	username text not null primary key,
+	userName text not null primary key,
 	password text not null,
-	name text not null,
 	email text not null,
 	firstName text not null,
 	lastName text not null,
@@ -10,28 +9,30 @@ create table users (
 );
 
 create table tokens (
-	username text not null primary key,
-	authorizationToken text not null
+	userName text not null primary key,
+	authToken text not null,
+	personID text not null
 );
 
 create table persons (
 	personID text not null primary key,
-	descendantUsername text not null,
+	descendant text not null,
 	firstName text not null,
 	lastName text not null,
 	gender text check (gender="f" or gender="m"),
-	fatherID text,
-	motherID text,
-	spouseID text
+	father text,
+	mother text,
+	spouse text
 );
 
 create table events (
 	eventID text not null primary key,
-	descendantUsername text not null,
-	personID text not null,s
+	descendant text not null,
+	personID text not null,
 	latitude real not null,
 	longitude real not null,
 	country text not null,
 	city text not null,
-	eventType text check (eventType="birth" or eventType="baptism" or eventType="christening" or eventType="marriage" or eventType="death")
+	eventType text check (eventType="birth" or eventType="baptism" or eventType="christening" or eventType="marriage" or eventType="death"),
+	year integer not null
 );
